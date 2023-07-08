3161edab924decf3b27888404c9399b1f2e2fb33
koldun <eg.sharypov@ya.ru>
2023-07-08 22:14:30 +0700

    prefabs and non-ready ai

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,67 @@
-using Unity.VisualScripting;
-using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+interface Decision {
+    void Do(Player self, Room room);
+}
+class MoveDecision : Decision {
+    public Vector2 target;
+    public MoveDecision(Vector2 target) {
+        this.target = target;
+    }
+    void Do(Player self, Room room) {
+    
+    }
+}
+class AttackDecision : Decision {
+    public GameObject target;
+    public AttackDecision(GameObject target){
+        this.target = target
+    }
+    void Do(Player self, PlayerWeapon playerWeapon, Room room){
+    
+    }
+}
+class
+public class Player : MonoBehaviour
+{
+    public GameObject room;
+    public class PlayerWeapon{
+        public 
+    }
+    Decision makeDesicion(){
+        boolean enemyFlag = false;
+        boolean lootFlag = false;
+        foreach(var child in room.transform){
+            if(child.tag == "Enemy"){
+                enemyFlag = true;
+                return new MoveDecision();
+                break;
+            }
+            else if(child.tag == "Loot"){
+                lootFlag = true
+                break;
+            }
+        }
+        return("Exit")
 
-public class Player : MonoBehaviour {
-    public float speed;
-    private void Start() {
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+    void Update()
+    {
+        makeDesicion().Do();
+        Transform targetEnemy = null;
+        float distanceToTargetEnemy = 99999.9;
+        foreach(var child in room.transform){
+           if(child.tag=="Enemy"){
+               var distance = Vector2.Distance(gameObject.transform.position, child.transform.position)
+               if(distanceToTargetEnemy>distance){
+                    targetEnemy = child;
+                    distanceToTargetEnemy = distance;
+                    enemiesOnBattlefield = true;
+               }
+           }
+        }
+        if enemiesOnBattlefield
+        if distanceToTargetEnemy<
     }
 }
